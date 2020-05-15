81c828fbc838833dd2931be5eae33ae812c2338a
André Luiz Rodrigues Campos Noguiera Soares <andresoaresti@gmail.com>
2020-05-15 11:07:39 -0300

    15: Erro no sample - FIX: Sample

--- Sample/src/Sample.java
+++ Sample/src/Sample.java
@@ -5,11 +5,7 @@ public class Sample {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
-		
-		
+		System.out.println("testando");
 	}
 
 }
